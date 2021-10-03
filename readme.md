d9e10facb060e64c3cf267cb88f7ebcb8dd94042
Jose Manuel <castellanogonzalezjosemanuel@gmail.com>
2021-10-03 16:43:31 +0200

    deploy

---
+++ readme.md
@@ -0,0 +1,46 @@
+# The game
+ 
+This game is the classic Snake game which consist in to eat pieces of fruit as the snake grows and dies if strikes with a wall or itself.
+
+## What is it
+
+This is a web page in the way of a video game. It is a grid of squares in where are pieces of fruit that are eated by a snake. 
+The snake fills squares in the grid as it grows eating. The way the game ends is to hit a wall in the border of the grid or to hit a square of the snake itself. As the snake eats the speed is increased.
+
+The functioning of the game is similar to the classic. This game, created in the middles of the 1970 decade, have lots of versions in multiple platforms,
+being maybe the most popular the Nokia 3310 Snake version, but still being versioned in modern platforms as Playstation 3, Xbox 360, PC, Mac OS, etc...
+
+The simple functioning and dificulty of this game makes an atractive and funny way to game, and interesting to programming as not with a very dificult level.
+
+This is a version of the most classic Snake, with the basics that makes this game great and fun.
+
+### Tecnologies
+
+This web page have been create with javascript, CSS and HTML tecnologies, and importing normalize to CSS to improbe browsers managing.
+
+## How to install
+
+clone the repository:
+
+git clone https://github.com/josemanuel80/the-game.git
+
+Get into the directory:
+
+cd the-game
+
+Actualize the repository:
+
+git pull origin master
+
+
+## How to run
+
+Open the HTML file in a browser.
+
+As the game start the snake is in the top left corner as a square and have to move right or down to beguin it movement.
+
+## Documentation
+
+[requirements](./docs/readme.md)
+
+https://josemanuel80.github.io/the-game/
---
+++ scripts/index.js
@@ -0,0 +1,235 @@
+// create the grid
+const width = 20;
+const height = 12;
+let score = 0;
+let cont = document.getElementById('h4');
+const grid = document.querySelector('.grid');
+const cells = [];
+for (i = 0; i < 240; i++) {
+  const cell = document.createElement('div', (id = i));
+  grid.appendChild(cell);
+  cells.push(cell);
+}
+
+//create the snake
+const snake = [];
+const addsnake = (index) => cells[index].classList.add('snake');
+const removesnake = (index) => cells[index].classList.remove('snake');
+const addFood = (index) => cells[index].classList.add('food');
+const removeFood = (index) => cells[index].classList.remove('food');
+let foodposition = Math.floor((Math.random() * Math.pow(10, 10)) % 240);
+headPosition = 106;
+let x = 6;
+let y = 5;
+let size = 4;
+let timer = 0;
+let timeUp = 500;
+let timeDown = 500;
+let timeLeft = 500;
+let timeRight = 500;
+addsnake(headPosition);
+function gameOver() {
+  if (window.confirm('Fin del juego. ¿Jugar otra vez?')) {
+    window.location.reload();
+  } else {
+    window.open('http://www.josemanuelcastellano.com', '_self');
+  }
+}
+addFood(foodposition);
+let lastDirection = 0;
+const handleKeyPress = (event) => {
+  event.preventDefault();
+  let direction = null;
+  direction = 0;
+  const { key } = event;
+  direction = key;
+  if (direction !== lastDirection) {
+    switch (direction) {
+      case 'ArrowUp':
+        repeatUp = true;
+        repeatDown = false;
+        repeatLeft = false;
+        repeatRight = false;
+        break;
+      case 'ArrowRight':
+        repeatUp = false;
+        repeatDown = false;
+        repeatLeft = false;
+        repeatRight = true;
+        break;
+      case 'ArrowLeft':
+        repeatUp = false;
+        repeatDown = false;
+        repeatLeft = true;
+        repeatRight = false;
+        break;
+      case 'ArrowDown':
+        repeatUp = false;
+        repeatDown = true;
+        repeatLeft = false;
+        repeatRight = false;
+        break;
+      default:
+        break;
+    }
+    function clearScreen() {
+      for (i = 0; i < 240; i++) {
+        removesnake(i);
+      }
+    }
+    window.setInterval(function () {
+      clearScreen();
+      switch (direction) {
+        case 'ArrowUp':
+          window.setTimeout(() => {
+            if (y >= 0 && repeatUp) {
+              y = y - 1;
+              if (y === -1) {
+                gameOver();
+              }
+              headPosition = headPosition - width;
+              if (snake.includes(headPosition)) {
+                gameOver();
+              }
+              snake.push(headPosition);
+              if (headPosition === foodposition) {
+                removeFood(foodposition);
+                score = score + 10;
+                cont.textContent = score;
+                foodposition = Math.floor(
+                  (Math.random() * Math.pow(10, 10)) % 240,
+                );
+                if (!snake.includes(foodposition)) {
+                  addFood(foodposition);
+                } else {
+                  foodposition = Math.floor(
+                    (Math.random() * Math.pow(10, 10)) % 240,
+                  );
+                  addFood(foodposition);
+                }
+                size++;
+              }
+              removesnake(headPosition);
+            }
+          }, timeUp);
+          timeUp = timeUp + 500;
+          break;
+        case 'ArrowRight':
+          window.setTimeout(() => {
+            if (x < 20 && repeatRight) {
+              x = x + 1;
+              headPosition++;
+              if (snake.includes(headPosition)) {
+                gameOver();
+              }
+              snake.push(headPosition);
+              if (headPosition === foodposition) {
+                removeFood(foodposition);
+                score = score + 10;
+                cont.textContent = score;
+                foodposition = Math.floor(
+                  (Math.random() * Math.pow(10, 10)) % 240,
+                );
+                if (!snake.includes(foodposition)) {
+                  addFood(foodposition);
+                } else {
+                  foodposition = Math.floor(
+                    (Math.random() * Math.pow(10, 10)) % 240,
+                  );
+                  addFood(foodposition);
+                }
+                size++;
+              }
+              if (x === 20) {
+                gameOver();
+              }
+              removesnake(headPosition);
+            }
+          }, timeRight);
+          timeRight = timeRight + 500;
+          break;
+        case 'ArrowDown':
+          window.setTimeout(() => {
+            if (y <= 11 && repeatDown) {
+              y = y + 1;
+              if (y === 12) {
+                gameOver();
+              }
+              headPosition = headPosition + width;
+              if (snake.includes(headPosition)) {
+                gameOver();
+              }
+              snake.push(headPosition);
+              if (headPosition === foodposition) {
+                removeFood(foodposition);
+                score = score + 10;
+                cont.textContent = score;
+                foodposition = Math.floor(
+                  (Math.random() * Math.pow(10, 10)) % 240,
+                );
+                if (!snake.includes(foodposition)) {
+                  addFood(foodposition);
+                } else {
+                  foodposition = Math.floor(
+                    (Math.random() * Math.pow(10, 10)) % 240,
+                  );
+                  addFood(foodposition);
+                }
+                size++;
+              }
+              removesnake(headPosition);
+            }
+          }, timeDown);
+          timeDown = timeDown + 500;
+          break;
+        case 'ArrowLeft':
+          window.setTimeout(() => {
+            if (x >= 0 && repeatLeft) {
+              x = x - 1;
+              headPosition--;
+              if (snake.includes(headPosition)) {
+                gameOver();
+              }
+              snake.push(headPosition);
+              if (headPosition === foodposition) {
+                removeFood(foodposition);
+                score = score + 10;
+                cont.textContent = score;
+                foodposition = Math.floor(
+                  (Math.random() * Math.pow(10, 10)) % 240,
+                );
+                if (!snake.includes(foodposition)) {
+                  addFood(foodposition);
+                } else {
+                  foodposition = Math.floor(
+                    (Math.random() * Math.pow(10, 10)) % 240,
+                  );
+                  addFood(foodposition);
+                }
+
+                size++;
+              }
+              if (x === -1) {
+                gameOver();
+              }
+              removesnake(headPosition);
+            }
+          }, timeLeft);
+          timeLeft = timeLeft + 500;
+          break;
+        default:
+          break;
+      }
+      if (snake.length === size) {
+        snake.splice(0, 1);
+      }
+      addsnake(headPosition);
+      snake.forEach((element) => {
+        addsnake(element);
+      });
+    }, timer);
+  }
+};
+cont.textContent = score;
+
+window.addEventListener('keydown', handleKeyPress);
